c774b4f085865e417641f883448045e9541f969c
Aapo Kyrola <aapo@protogeo.com>
2014-02-17 22:36:13 -0800

    added

---
+++ analysis/analyze_fof.py
@@ -0,0 +1,21 @@
+'''
+Analyze inout file
+'''
+
+import os
+import sys
+
+
+f = sys.argv[-1]
+
+data = open(f).read()
+
+values = [[float(x) for x in ln.split("\t")] for ln in data.split("\n")[1:] if len(ln) > 1]
+
+fofchecksum = sum([d[0] for d in values])
+totalfoftime = sum([d[1] for d in values])
+
+
+
+print f
+print "  FOF: time=%f secs,  %f per query, %f queries per sec, checksum=%f" % (totalfoftime * 0.000001, totalfoftime * 0.000001 / len(values), len(values ) / (totalfoftime * 0.000001), fofchecksum)
